59d2d52f5ac8862c7b32e23787f445e50309d657
Jeremiah Kwarteng <jlkwarteng@gmail.com>
2023-07-15 10:23:53 +0000

    Chapter 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   content: [
-    "./app/views/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/assets/stylesheets/**/*.css",
-    "./app/javascript/**/*.js",
-  ],
-  plugins: [require("@tailwindcss/forms")],
-};
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ]
+}
